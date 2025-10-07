2b3d7672a0f13bd223242d40e8c731dfc7bb38e0
Jefte SG <jefte.green@gmail.com>
2025-10-06 23:08:32 -0300

    Add CPF and gender validation to DTOPessoa serializer
    - Implemented CPF validation to ensure valid format.
    - Added gender validation to restrict values to 'M' or 'F', converting input to uppercase.
    - Removed commented-out create method for clarity.

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
 from .models import Pessoa
+from .cpf import CPF
 
 class DTOPessoa(serializers.ModelSerializer):
     class Meta:
         model = Pessoa
         fields = "__all__"
 
-        
-    # def create(self, validated_data):
-    #     return Pessoa.objects.create(**validated_data)
+    def validate_cpf(self, value):
+        cpf = CPF(value)
+        if not cpf.validate():
+            raise serializers.ValidationError("CPF inválido.")
+        return value
+
+    def validate_sexo(self, value):
+        if value.upper() not in ['M', 'F']:
+            raise serializers.ValidationError('Sexo deve ser "M" ou "F".')
+        return value.upper()
+
     
