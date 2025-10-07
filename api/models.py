ffa1bccb1cf29c6a44fa3f0470ad6fe66e45af1a
Jefte SG <jefte.green@gmail.com>
2025-10-06 23:08:25 -0300

    Refactor Pessoa model to enforce gender validation and improve field choices. Added SEXO_CHOICES for 'M' and 'F' options, and removed custom validation logic for CPF and sexo.

--- api/models.py
+++ api/models.py
@@ -9,21 +9,14 @@ class Pessoa(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100)
     data_nascimento = models.DateField(null=True)
-    sexo = models.CharField(max_length=1)
+    SEXO_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+    ]
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
     cpf = models.CharField(max_length=11)
     peso = models.FloatField()
     altura = models.FloatField()
-
-    def clean(self): 
-        cpf = CPF(self.cpf)
-        if not cpf.validate():
-            raise ValidationError({'cpf': 'CPF inválido'}) 
-        
-        if self.sexo not in ['M', 'F']:
-            raise ValidationError({'sexo': 'Sexo deve ser "M" ou "F"'})
-
-
-    
     def calcular_peso_ideal(self):
 
         if self.sexo == 'M':
